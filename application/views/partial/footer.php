f7cd652c03b1e1643050187929406b601f8e162d
castomlingi <38857394+castomlingi@users.noreply.github.com>
2018-04-30 15:55:09 +0300

    pos update

---
+++ application/views/partial/footer.php
@@ -0,0 +1,15 @@
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="jumbotron push-spaces">
+			
+  			<?php echo 'Point Of Sale System'; ?> 
+			<?php echo 'common please visit my'; ?>
+			<a href="https://hatuamoja.com" target="_blank">
+			<?php echo 'ICT Company' ; ?></a>
+			<?php echo 'common learn about Hatua Moja'; ?>
+		</div>
+	</div>
+</body>
+</html>
